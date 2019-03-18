e6d799a0e489ae31998bfde3e3c394e916aa4904
Britta Rodenbeck <cheers@brittarodenbeck.com>
2019-03-18 17:31:48 -0400

    slinging json tutorials

---
+++ starter-files/json-homework/main.js
@@ -0,0 +1,20 @@
+const books = [
+  {
+    "name": "Harry Potter and the Goblet of Fire",
+    "author": "J. K. Rowling",
+    "year": 2000,
+    "genre": "Fantasy Fiction",
+  },
+  {
+    "name": "Harry Potter and the Sorcerer's Stone",
+    "author": "J. K. Rowling",
+    "year": 1998,
+    "genre": "Fantasy Fiction",
+  },
+  {
+    "name": "Harry Potter and the Chamber of Secrets",
+    "author": "J. K. Rowling",
+    "year": 1999,
+    "genre": "Fantasy Fiction",
+  }
+]
