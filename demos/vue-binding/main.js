e85bb21ab2aa64363f56532fd0fdadfd35509c2d
Britta Rodenbeck <cheers@brittarodenbeck.com>
2019-04-20 10:21:24 -0400

    :sparkles: adding a vue data binding demo

---
+++ demos/vue-binding/main.js
@@ -0,0 +1,13 @@
+const $dynamicInput = document.querySelector('#dynamicInput');
+const data = {
+  message: 'Hello, world.',
+};
+
+$dynamicInput.value = data.message;
+
+// new Vue({
+//   el: '#root',
+//   data: {
+//     message: 'Hello, world.',
+//   },
+// });
